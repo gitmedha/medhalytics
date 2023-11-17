5ba5c9b5ea10b58b72d58c770adabf291c73d2cd
RaguramanH <Raguraman.hari@medha.org.in>
2023-11-17 14:55:25 +0530

    added and removed columns

--- models/student_enrollment.sql
+++ models/student_enrollment.sql
@@ -165,9 +165,9 @@ programs_cte AS (
 
 -- Main query combining the CTEs
 SELECT
-    users1_cte."username" AS "program_updated_by_user",
-  users2_cte."username" AS "program_created_by_user",
-  users_cte."username" AS "institution_assigned_to_user",
+    users1_cte. "program_updated_by_user",
+    users2_cte."program_created_by_user",
+    users_cte."institution_assigned_to_user",
     cte_students."student_SIS_id",
     cte_students."Source",
     cte_program_enrollments."status",
