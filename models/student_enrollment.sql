881f117af4e848c872153367a0d316ed4fbaa1e7
RaguramanH <Raguraman.hari@medha.org.in>
2023-11-17 13:01:29 +0530

    added and removed columns

--- models/student_enrollment.sql
+++ models/student_enrollment.sql
@@ -154,7 +154,7 @@ LEFT JOIN batches_cte ON cte_program_enrollments.batch = batches_cte.batches_id
 LEFT JOIN institutions_cte as i1 ON cte_program_enrollments.institution = i1.institutions_id
 LEFT JOIN users_cte ON i1.institutions_assigned_to = users_cte.user_id
 LEFT JOIN grants_cte ON batches_cte.batches_grant = grants_cte.grants_id
-LEFT JOIN programs_cte as p1 ON batches_cte.batches_program = p1.programs_id
+LEFT JOIN programs_cte ON batches_cte.batches_program = programs_cte.programs_id
 LEFT JOIN programs_cte as p2 ON users_cte.user_id = p2.programs_updated_by
 LEFT JOIN programs_cte as p3 ON users_cte.user_id = p3.programs_created_by
 Order by cte_program_enrollments.id
